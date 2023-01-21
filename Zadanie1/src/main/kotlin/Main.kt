a1d22cbeb68173d0e342cffed7943f5b2ac9ab03
Кристина <106802110+1-Albeda-1@users.noreply.github.com>
2023-01-21 15:48:17 +0300

    Add files via upload

---
+++ Zadanie1/src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+//Задание №1
+//Создать приложение, которое подсчитывает количество подряд идущих одинаковых символов
+//во введенной строке. На вход подается, например, строка AAADSSSRRTTHAAAA.
+//На выходе получаем A3DS3R2T2HA4.
+//То есть, если количество подряд идущих символов меньше двух, то мы не пишем единицу.
+fun main(args: Array<String>)
+{
+    println("Задание №1")
+    print("Введите строку:")
+    var str:String = String()
+    str = readln()
+    var res = ""
+    var c:Char = str[0]
+    var count:Int = 0
+    for(x in str){
+        if(c!=x)
+        {
+            res+=c+if(count.toString()!="1") count.toString() else ""
+            count = 0
+        }
+        count++
+        c = x
+    }
+    res+=c+if(count.toString()!="1") count.toString() else ""
+    println("Преобразованная строка:$res")
+}
---
+++ Zadanie2/src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+//Задание №2
+//Создать приложение, которое подсчитывает количество различных символов во введенной строке.
+//Символы в ответе расположить в алфавитном порядке. Например, дана строка AASADDSS.
+//На выходе получаем:
+//A - 3
+//D - 2
+//S - 3
+fun main(args: Array<String>) {
+    println("Задание №1")
+    print("Введите строку:")
+    var str:String = String()
+    str = readln()
+    var count:Int = 0
+    val c = str.toCharArray()
+    str = c.sorted().joinToString( "") + " "
+    var res = str[0]
+
+    for(x in str)
+    {
+        if(x == res)
+        {
+            count++
+        }
+        else
+        {
+            println(res+" - "+count.toString())
+            res = x
+            count = 1
+        }
+    }
+
+
+}
